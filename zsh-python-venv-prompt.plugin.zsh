c6e45757122826216077627ee74caed3ea9a0d4b
alessandro stoppato <alexrah@gmail.com>
2023-08-10 21:07:23 +0200

    zsh-python-venv-prompt.plugin.zsh: fix mismatching backticks

--- zsh-python-venv-prompt.plugin.zsh
+++ zsh-python-venv-prompt.plugin.zsh
@@ -2,7 +2,7 @@ function python_venv_prompt(){
   [[ -n ${VIRTUAL_ENV} ]] || return
 
   VIRTUALENV_BASE=`echo $VIRTUAL_ENV | awk -F'/' '{if($NF == ".venv") print $(NF-1); else print $NF }'`
-  VIRTUALENV_PYVERSION=`python --version | awk '{print "-py"$2}'
+  VIRTUALENV_PYVERSION=`python --version | awk '{print "-py"$2}'`
 
   echo " ${ZSH_THEME_VENV_PROMPT_PREFIX=[}${ZSH_THEME_VENV_BASENAME=$VIRTUALENV_BASE}${ZSH_THEME_VENV_PYVERSION=$VIRTUALENV_PYVERSION}${ZSH_THEME_VENV_PROMPT_SUFFIX=]}"
 }
